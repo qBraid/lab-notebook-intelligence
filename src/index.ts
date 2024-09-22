66cbdc7cf685f5bb0de8becf5e81eb84c238d2a1
Mehmet Bektas <mbektasgh@outlook.com>
2024-09-22 14:53:01 -0700

    initialize extension from template

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
+import { requestAPI } from './handler';
+
+/**
+ * Initialization data for the @mbektas/jupyter-notebook-intelligence extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: '@mbektas/jupyter-notebook-intelligence:plugin',
+  description: 'Jupyter Notebook Intelligence extension',
+  autoStart: true,
+  optional: [ISettingRegistry],
+  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
+    console.log('JupyterLab extension @mbektas/jupyter-notebook-intelligence is activated!');
+
+    if (settingRegistry) {
+      settingRegistry
+        .load(plugin.id)
+        .then(settings => {
+          console.log('@mbektas/jupyter-notebook-intelligence settings loaded:', settings.composite);
+        })
+        .catch(reason => {
+          console.error('Failed to load settings for @mbektas/jupyter-notebook-intelligence.', reason);
+        });
+    }
+
+    requestAPI<any>('get-example')
+      .then(data => {
+        console.log(data);
+      })
+      .catch(reason => {
+        console.error(
+          `The jupyter_notebook_intelligence server extension appears to be missing.\n${reason}`
+        );
+      });
+  }
+};
+
+export default plugin;
